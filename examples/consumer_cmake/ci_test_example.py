88b5a26573ec49d457506508cb917c5175f740fe
danimtb <danimanzaneque@gmail.com>
2026-04-30 09:16:15 +0200

    simplify tests

--- examples/consumer_cmake/ci_test_example.py
+++ examples/consumer_cmake/ci_test_example.py
@@ -1,6 +1,4 @@
-import os
-
 from test.examples_tools import run
 
 run("conan install -s compiler.cppstd=17 --build=missing")
-run("conan build -s compiler.cppstd=17")
+run("conan build")
--- examples/pose_estimation/ci_test_example.py
+++ examples/pose_estimation/ci_test_example.py
@@ -3,13 +3,8 @@ import platform
 from test.examples_tools import run
 
 run("conan install -s compiler.cppstd=17 --build=missing")
-run("conan build -s compiler.cppstd=17 --build=missing")
-
-
-run("python -m pip install -q --upgrade pip colcon-common-extensions")
-run("conan install -s compiler.cppstd=17 --build=missing")
 
 if platform.system() == "Windows":
-  run("cmd.exe //C \"call build/Release/generators/conanrosenv.bat && cmake --preset conan-release && cmake --build --preset conan-release")
+    run("call build/Release/generators/conanbuild.bat && cmake --preset conan-release && cmake --build --preset conan-release")
 else:
-  run("source build/Release/generators/conanrosenv.sh & cmake --preset conan-release && cmake --build --preset conan-release")
+    run(". ./build/Release/generators/conanbuild.sh && cmake --preset conan-release && cmake --build --preset conan-release")
