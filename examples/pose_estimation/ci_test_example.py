050ff7ab5c23ca08eabc17514b7ede6545a53382
danimtb <danimanzaneque@gmail.com>
2026-04-30 15:32:42 +0200

    fix examples

--- examples/pose_estimation/ci_test_example.py
+++ examples/pose_estimation/ci_test_example.py
@@ -5,6 +5,6 @@ from test.examples_tools import run
 run("conan install -s compiler.cppstd=17 --build=missing")
 
 if platform.system() == "Windows":
-    run("call build/Release/generators/conanbuild.bat && cmake --preset conan-release && cmake --build --preset conan-release")
+    run("call ./build/Release/generators/conanbuild.bat && cmake --preset conan-release && cmake --build --preset conan-release")
 else:
     run(". ./build/Release/generators/conanbuild.sh && cmake --preset conan-release && cmake --build --preset conan-release")
